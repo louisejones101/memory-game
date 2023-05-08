e22de6e461d53add44a52078e3905d554f23e716
louisejones101 <louisejonesart@gmail.com>
2023-05-08 23:02:09 +0000

    a function to flip the card was created, function gets the data-id of the card when clicked.  eventListener added to the createBoard function and flipcard added to the event listener.  console log used to check that the click works and also that the cardId and name are being retrieved ewhen the card is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // The array that is going to hold the car images
-const cardArray = [  
+const birdCards = [  
 {
     name: 'bird1', 
     imgSrc: 'assets/images/bird1.png',
@@ -34,19 +34,24 @@ const cardArray = [
 },
 ];
 
-cardArray.sort(()=> 0.5 - Math.random());  //randomly sort the images in the array
+birdCards.sort(()=> 0.5 - Math.random());  //randomly sort the images in the array
 
 const gridDisplay = document.querySelector('#game-grid');
 
 function createBoard () {
-    for (let i = 0; i < cardArray.length; i++){  //for loop to iterate through the array
+    for (let i = 0; i < birdCards.length; i++){  //for loop to iterate through the array
         const card = document.createElement('img')
         card.setAttribute('src', 'assets/images/cardBack.png');
         card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
         gridDisplay.append(card);
-       console.log(card, i);
-    }
-    
+    }  
 }
 
 createBoard () //call createBoard function
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id');
+    console.log(birdCards[cardId].name);
+    console.log('clicked', cardId);
+}
