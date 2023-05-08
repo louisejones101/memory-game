c07434c63aafc7dc88c3d426d0c08c1da5cc8240
louisejones101 <louisejonesart@gmail.com>
2023-05-08 19:57:29 +0000

    Card Array and the card objects containing the image names and file links set up in script.js. Math random to sort through the cardArray also added

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// The array that is going to hold the car images
+const cardArray = [  
+{
+    name: 'bird1', 
+    img: 'images/bird1.png',
+},
+{
+    name: 'bird2', 
+    img: 'images/bird2.png',
+},
+{
+    name: 'bird3', 
+    img: 'images/bird3.png',
+},
+{
+    name: 'bird4', 
+    img: 'images/bird4.png',
+},
+{
+    name: 'bird5', 
+    img: 'images/bird5.png',
+},
+{
+    name: 'bird6', 
+    img: 'images/bird6.png',
+},
+{
+    name: 'bird7', 
+    img: 'images/bird7.png',
+},
+{
+    name: 'bird8', 
+    img: 'images/bird8.png',
+},
+]
+
+cardArray.sort(()=> 0.5 - Math.random());
