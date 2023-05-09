b63717167fb604f8776fac5973573e94a4af40f2
louisejones101 <louisejonesart@gmail.com>
2023-05-09 14:11:45 +0000

    if statement added to the checkMatch function and temporary alerts added to see that the if statement works

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,38 @@ const birdCards = [
     name: 'bird8', 
     img: 'assets/images/bird8.png',
 },
+{
+    name: 'bird1', 
+    img: 'assets/images/bird1b.png',
+},
+{
+    name: 'bird2', 
+    img: 'assets/images/bird2.png',
+},
+{
+    name: 'bird3', 
+    img: 'assets/images/bird3.png',
+},
+{
+    name: 'bird4', 
+    img: 'assets/images/bird4.png',
+},
+{
+    name: 'bird5', 
+    img: 'assets/images/bird5.png',
+},
+{
+    name: 'bird6', 
+    img: 'assets/images/bird6.png',
+},
+{
+    name: 'bird7', 
+    img: 'assets/images/bird7.png',
+},
+{
+    name: 'bird8', 
+    img: 'assets/images/bird8.png',
+},
 ];
 
 birdCards.sort(()=> 0.5 - Math.random());  //randomly sort the images in the array
@@ -52,8 +84,14 @@ function createBoard () {
 
 createBoard () //call createBoard function
 
+//function to check if there is a match
 function checkMatch() {
-    console.log('checking for match');
+   if (cardsPicked[0] === cardsPicked[1]){
+        alert('You have found a match!');
+     } else {
+        console.log('oh dear no match');
+     }
+
 }
 
 // flip card function
@@ -61,8 +99,8 @@ function flipCard() {
     const cardId = this.getAttribute('data-id'); //fetches the data-id for each card
     cardsPicked.push(birdCards[cardId].name); //pushes the cards that have been picked to a new array cardsPicked
     this.setAttribute('src', birdCards[cardId].img);
-    if(cardsPicked.length == 2){  
-        setTimeout(checkMatch, 500); //set a slight delay when two cards clicked before checking for match
+    if(cardsPicked.length === 2){  
+        setTimeout(checkMatch, 500); //set a slight delay when two cards clicked before calling checkMatch function
     }
 }
 
